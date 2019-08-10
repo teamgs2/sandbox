f76679736a30051e50695c0cee12f7e57a8ca086
Eric THAO <ericthao@macbook-pro-de-eric.home>
2019-08-10 22:51:08 +0200

    remove boo

--- Orange-Boosted-Bootstrap-4.3.1/js/tests/integration/bundle.js
+++
@@ -1,8 +0,0 @@
-import 'popper.js'
-import $ from 'jquery'
-import boosted from '../../../dist/js/boosted'
-
-$(() => {
-  $('#resultUID').text(boosted.Util.getUID('bs'))
-  $('[data-toggle="tooltip"]').tooltip()
-})
